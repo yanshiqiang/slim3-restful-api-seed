4d2a314c5e5737bc331c946785727d3720ac7d04
Dyer, Andy <andrew.dyer@sky.uk>
2018-01-26 20:18:44 +0000

    Added JWT middleware

--- bootstrap/middleware.php
+++ bootstrap/middleware.php
@@ -10,7 +10,23 @@ use App\Middleware\JsonResponseMiddleware;
 
 $App->add(new JsonResponseMiddleware($container));
 
-// Jwt Auth
-use App\Middleware\JwtAuthMiddleware;
+// Jwt Authentication
+use Slim\Middleware\JwtAuthentication;
 
-$App->add(new JwtAuthMiddleware($container));
+$App->add(new JwtAuthentication([
+    'algorithm' => env('JWT_ALGORITHM', 'HS256'),
+    'callback' => function ($request, $response, $arguments) use ($container) {
+        $container['jwt'] = $arguments['decoded'];
+    },
+    'error' => function ($request, $response, $arguments) {
+        $data['status'] = 'error';
+        $data['message'] = $arguments['message'];
+        $body = json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
+        return $response->withHeader('Content-Type', 'application/json')->write($body);
+    },
+    'passthrough' => ['/auth'],
+    'path' => '/',
+    'relaxed' => ['localhost'],
+    'secret' => env('JWT_SECRET'),
+    "secure" => env('JWT_SECURE', true)
+]));
