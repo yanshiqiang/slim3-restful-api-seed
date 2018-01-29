1637b77d6d4a98a6c94464cfe062da4a8ed10e2d
Dyer, Andy <andrew.dyer@sky.uk>
2018-01-29 11:34:46 +0000

    Updated AuthController to just jwt config.

--- config/jwt.php
+++ config/jwt.php
@@ -3,9 +3,14 @@
 return [
     'jwt' => [
         'algorithm' => env('JWT_ALGORITHM', 'HS256'),
-        'passthrough' => ['/auth'],
+        'passthrough' => [
+            '/auth'
+        ],
         'path' => '/',
-        'relaxed' => ['localhost'],
+        'relaxed' => [
+            '127.0.0.1',
+            'localhost'
+        ],
         'secret' => env('JWT_SECRET', ''),
         'secure' => env('JWT_SECURE', true),
         'ttl' => env('JWT_TOKEN_TTL')
