5bb2ba5807304247312dc9637ab652f8a76018ee
Dyer, Andy <andrew.dyer@sky.uk>
2018-02-05 16:36:29 +0000

    Added JWT configs as env variables.

--- config/jwt.php
+++ config/jwt.php
@@ -3,14 +3,9 @@
 return [
     'jwt' => [
         'algorithm' => env('JWT_ALGORITHM', 'HS256'),
-        'passthrough' => [
-            '/auth'
-        ],
-        'path' => ['/'],
-        'relaxed' => [
-            '127.0.0.1',
-            'localhost'
-        ],
+        'passthrough' => envStringToArray(env('JWT_PASSTHROUGH'), ''),
+        'path' => envStringToArray(env('JWT_PATH', '/')),
+        'relaxed' => envStringToArray(env('JWT_RELAXED', 'localhost')),
         'secret' => env('JWT_SECRET', ''),
         'secure' => env('JWT_SECURE', true),
         'ttl' => env('JWT_TOKEN_TTL')
